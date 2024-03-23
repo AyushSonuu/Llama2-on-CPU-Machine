274ac15850391c5003a7218ad4ad28a66a2257f9
sonuayush <sonuayush55@gmail.com>
2024-03-23 11:22:23 +0530

    project setup structure done

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Llama2-on-CPU-Machine
+
+### step 1: 
+ ` git clone https://github.com/AyushSonuu/Llama2-on-CPU-Machine `
+
+### step 2: 
+`conda create -n cpullama python==3.10 -y`
+
+### step 3:
+`conda activate cpullama`
+
+### step 4:
+`pip install -r requirements.txt`
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Deep-Learning-Project',
+    version='1.0.0',
+    packages=['src'],
+    url='https://github.com/AyushSonuu/Llama2-on-CPU-Machine',
+    license='MIT',
+    author='AYUSH',
+    author_email='ayush.sonu@impressico.com',
+)
